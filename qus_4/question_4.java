6072426ffc7e1c15659ea487f8f11c0242479469
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-01 16:10:10 +0530

    4 th question added

---
+++ qus_4/question_4.java
@@ -0,0 +1,15 @@
+package qus_4;
+
+import java.util.Scanner;
+
+public class question_4 {
+    public static void main(String[] args) {
+        Scanner number=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int a=number.nextInt();
+      System.out.println((a%2==0)?"even":"odd");  
+        
+
+    }
+    
+}
