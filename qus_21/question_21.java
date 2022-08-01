9558119b41ab68306513d915f4f841183ef93c8b
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-02 02:02:45 +0530

    Add other answers

--- qus_21/question_21.java
+++ qus_21/question_21.java
@@ -1,5 +1,47 @@
 package qus_21;
 
+import java.util.Scanner;
+
 public class question_21 {
-    
+    public static void grade_points()
+        {
+            int number1 = GetInteger("Enter your marks:");
+            
+            if(number1>=90)
+            {
+                System.out.println("Your Grade is A");
+            }
+            else if(number1>=80)
+            {
+                System.out.println("Your Grade is B");
+            }
+            else if (number1 >= 60)
+            {
+                System.out.println("Your Grade is c");
+            }
+            else if (number1 >= 50)
+            {
+                System.out.println("Your Grade is D");
+            }
+            else if (number1 < 50)
+            {
+                System.out.println("Your Grade is F");
+            }
+
+        }
+        public static void main(String[] args) {
+           
+            grade_points();
+        }
+    public static int GetInteger(String message)
+    {
+        printmessage(message);
+        Scanner sc=new Scanner(System.in);
+        int temp=sc.nextInt();
+        return temp;
+    }
+    public static void printmessage(String message)
+    {
+        System.out.println(message);
+    }
 }
--- qus_25/question_25.java
+++ qus_25/question_25.java
@@ -1,5 +1,53 @@
 package qus_25;
 
+import java.util.Scanner;
+
 public class question_25 {
-    
+    public static void main(String[] args) {
+        
+        menu_drive();
+    }
+    public static void menu_drive()
+        {
+            System.out.println("Add the two numbers Enter 1:");
+            System.out.println("subtraction the two numbers Enter 2:");
+            System.out.println("Multiplication of two numbers Enter  3:");
+            System.out.println("Division of two numbers Enter  4:");
+            System.out.println("Modulo division of two numbers Enter 5:");
+            int number = GetInteger("Enter the Number 1 to 5 :");
+            int number1 = GetInteger("Enter your first number for your calculation:");
+            int number2 = GetInteger("Enter your second number for your claculation:");
+            switch(number)
+            {
+                case 1:
+                //return number1+number2;
+                System.out.println( number1+number2);
+                break;
+                case 2:
+                System.out.println(number1-number2);
+                break;
+                case 3:
+                System.out.println(number1*number2);
+                break;
+                case 4:
+                System.out.println(number1/number2);
+                break;
+                case 5:
+                System.out.println(number1%number2);
+                break;
+
+            }
+            
+}
+public static int GetInteger(String message)
+{
+    printmessage(message);
+    Scanner sc=new Scanner(System.in);
+    int temp=sc.nextInt();
+    return temp;
+}
+public static void printmessage(String message)
+{
+    System.out.println(message);
+}
 }
