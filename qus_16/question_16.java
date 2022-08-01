53091a8132da038736cdeebe937a6b951e29d4f6
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-02 01:18:00 +0530

    Added other questions

--- qus_16/question_16.java
+++ qus_16/question_16.java
@@ -1,5 +1,37 @@
 package qus_16;
 
+import java.util.Scanner;
+
 public class question_16 {
+    public static void vowels_check()
+    {
+        char name = GetChar("Enter the Any chars :");
+       
+        if (name == 'a' || name == 'e' || name == 'i' || name == 'o' || name == 'u'||name =='A' ||name=='E'||name=='I'||name=='O'||name=='U')
+        {
+            System.out.println("Yes this char is vowel");
+            
+    
+        }
+        else
+        {
+            System.out.println("This char is constant");
+        }
+    }
+    public static void main(String[] args) {
+        vowels_check();
+        
+    }
+    public static char GetChar(String message)
+    {
+        PrintMessage(message);
+        Scanner sc=new Scanner(System.in);
+        char temp=sc.next().charAt(0);
+        return temp;
+    }
+    public static void PrintMessage(String message)
+    {
+        System.out.println(message);
+    }
     
 }
--- qus_9/question_9.java
+++ qus_9/question_9.java
@@ -1,7 +1,42 @@
 package qus_9;
 
+import java.util.Scanner;
+
 public class question_9 {
-    public static void main(String[] args) {
+    public static void smallest_ofthree_numbers()
+    {
+        int number1 = GetInteger("Enter the numbers: ");
+       
+        int number2 = GetInteger("Enter the numbers: ");
         
+        int number3 = GetInteger("Enter the numbers: ");
+       
+        if (number1 < number2)
+        {
+            System.out.println(number1 + ":Number1 is Small");
+
+        }
+        else if (number2 < number3)
+        {
+            System.out.println(number2 + ":Number2 is Small");
+        }
+        else if (number3 < number1)
+        {
+            System.out.println(number3 + "Number3 is Small");
+        }
+    }
+    public static void main(String[] args) {
+        smallest_ofthree_numbers();
+    }
+    public static int GetInteger(String message)
+    {
+        printmessage(message);
+        Scanner sc=new Scanner(System.in);
+        int temp=sc.nextInt();
+        return temp;
+    }
+    public static void printmessage(String message)
+    {
+        System.out.println(message);
     }
 }
