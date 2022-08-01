b04f6e1f2e4794d71f71db1bf40f93dd1bd4e839
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-01 23:47:55 +0530

    Add other question

--- qus_14/question_14.java
+++ qus_14/question_14.java
@@ -1,8 +1,38 @@
 package qus_14;
 
+import java.util.Scanner;
+
 public class question_14 {
+    public static void Triangle()
+    {
+        int Angle1=Getint("Enter the Angle1:");
+        int Angle2=Getint("Enter the Angle2:");
+        int Angle3=Getint("Enter the Angle3:");
+        int sum_Angles=Angle1+Angle2+Angle3;
+
+        if(sum_Angles==180)
+        {
+            System.out.println("Yes Triangle");
+        }else{
+            System.out.println("This not Triangle");
+        }
+    }
     public static void main(String[] args) {
         
+            Triangle();
+        
+        
+    }
+    public static int Getint(String message)
+    {
+        PrintMessage(message);
+        Scanner obj=new Scanner(System.in);
+        int temp=obj.nextInt();
+        return temp;
+    }
+    public static void PrintMessage(String message)
+    {
+        System.out.println(message);
     }
     
 }
--- qus_22/question_22.java
+++ qus_22/question_22.java
@@ -2,4 +2,8 @@ package qus_22;
 
 public class question_22 {
     
+    public static void main(String[] args) {
+        chess_bord();
+    }
+    
 }
